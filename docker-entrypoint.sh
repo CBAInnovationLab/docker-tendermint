8430bed31cfea64a1a5f391406fb329a2a8e3909
Dan Turner <dan.p.turner@gmail.com>
2017-12-04 23:51:58 +1100

    Quote environment variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,13 +3,13 @@
 mkdir -p ~/.tendermint
 
 if [ -n "$TENDERMINT_CONFIG_PATH" ]; then
-  cp -f $TENDERMINT_CONFIG_PATH ~/.tendermint/config.toml
+  cp -f "$TENDERMINT_CONFIG_PATH" ~/.tendermint/config.toml
 fi
 if [ -n "$TENDERMINT_GENESIS_PATH" ]; then
-  cp -f $TENDERMINT_GENESIS_PATH ~/.tendermint/genesis.json
+  cp -f "$TENDERMINT_GENESIS_PATH" ~/.tendermint/genesis.json
 fi
 if [ -n "$TENDERMINT_PRIV_VALIDATOR_PATH" ]; then
-  cp -f $TENDERMINT_PRIV_VALIDATOR_PATH ~/.tendermint/priv_validator.json
+  cp -f "$TENDERMINT_PRIV_VALIDATOR_PATH" ~/.tendermint/priv_validator.json
 fi
 
 tendermint node --p2p.seeds "$TENDERMINT_SEEDS" --moniker "`hostname`" --rpc.laddr "$TENDERMINT_ADDR"
